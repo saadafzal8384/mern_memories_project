5edcd9ce6a2fed91145115a27adee7b48b39d7d6
Saad <mu.saadafzal@gmail.com>
2022-04-10 20:56:19 +0100

    Added ENV and constants Files

--- server/index.js
+++ server/index.js
@@ -2,21 +2,23 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 
 import postRoutes from "./routes/posts.js";
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL =
-  "mongodb+srv://admin:1234@cluster0.2uldf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+//const CONNECTION_URL =
+("mongodb+srv://admin:1234@cluster0.2uldf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
 const PORT = process.env.PORT || 5000;
 mongoose
-  .connect(CONNECTION_URL)
+  .connect(process.env.CONNECTION_URL)
   .then(() =>
     app.listen(PORT, () =>
       console.log(`Server Running on Port: http://localhost:${PORT}`)
