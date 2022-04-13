e152edc57456deff9549ff600ae62f96a1323a96
Saad <mu.saadafzal@gmail.com>
2022-04-13 21:39:57 +0100

    Added Pagination for Memories List

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,12 +1,24 @@
 import React, { useState, useEffect } from "react";
-import { Container, Grow, Grid } from "@material-ui/core";
+import {
+  Container,
+  Grow,
+  Grid,
+  GridAppBar,
+  TextField,
+  Button,
+  Paper,
+} from "@material-ui/core";
 import Posts from "../Posts/Posts";
 import Form from "../Form/Form";
 import { useDispatch } from "react-redux";
 import { getPosts } from "../../actions/posts";
+import Pagination from "../Pagination";
+import useStyles from "./styles";
+
 const Home = () => {
   const [currentId, setCurrentId] = useState(0);
   const dispatch = useDispatch();
+  const classes = useStyles();
 
   useEffect(() => {
     dispatch(getPosts());
@@ -25,6 +37,9 @@ const Home = () => {
           </Grid>
           <Grid item xs={12} sm={4}>
             <Form currentId={currentId} setCurrentId={setCurrentId} />
+            <Paper className={classes.pagination} elevation={6}>
+              <Pagination />
+            </Paper>
           </Grid>
         </Grid>
       </Container>
